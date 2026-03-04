f6b3ee2bf9f9523434300a3ab1321602297801fc
Trần Anh Tuấn <tohonmoingay@gmail.com>
2026-03-04 20:32:19 +0700

    Thêm danh sách 1

---
+++ lib/features/welcome-screen/presentation/pages/welcome_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Text(
+            'Lập trình viên',
+          ),
+        ),
+      ),
+    );
+  }
+}
